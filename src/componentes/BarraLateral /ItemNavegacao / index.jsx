76397dad8ae7b581494ab69efbd5804375f384d8
Gabriel Paulo Rodrigues <gabrielpaulogpsc@gmail.com>
2024-10-21 15:02:17 -0300

    Finalizando o curso

--- src/componentes/BarraLateral /ItemNavegacao / index.jsx
+++
@@ -1,8 +0,0 @@
-const ItemNavegacao = ( {children, IconeAtivo, IconeInativo, ativo = false} ) => {
-    return <> 
-        <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
-        {children}
-    </>
-}
-
-export default ItemNavegacao
