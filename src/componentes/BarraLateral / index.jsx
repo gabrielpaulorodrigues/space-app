cd4ab7d4bc3863f93585ecbee710079f428e1863
gabrielpaulorodrigues <gabrielpaulogpsc@gmail.com>
2024-10-21 10:51:58 -0300

    iniciando aula 2

---
+++ src/componentes/BarraLateral / index.jsx
@@ -0,0 +1,23 @@
+import { styled } from "styled-components"
+
+const ListaEstilizada = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px; 
+
+`
+
+const BarraLateral = () =>{
+    return (
+        <aside>
+            <nav>
+                <ListaEstilizada>
+
+                </ListaEstilizada>
+            </nav>
+        </aside>
+    )
+}
+
+export default BarraLateral
